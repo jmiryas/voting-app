344b36467be8993dab76a6ddfc210c6082019476
Rizky Ramadhan <jmiryas@gmail.com>
2022-09-18 09:14:57 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                "transparent": "transparent",
+                "current": "currentColor",
+                "black": colors.black,
+                "white": colors.white,
+                "gray": colors.trueGray,
+                "gray-background": "#f7f8fc",
+            },
+            fontFamily: {
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
